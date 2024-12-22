37ea0560b5a1b183008a6a6a1813b71b6a78cd27
Ruchita-20 <ruchitadalvi20@gmail.com>
2024-12-22 20:14:23 +0530

    Base of signin

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import LoginPage from "@/page";
+function App() {
+  return (
+    <>
+    <LoginPage/>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
